6ffffdd731a3d0e0e618a3f9ae7a8820387e60d4
Jonathan Fotland <jonathan@agora.io>
2019-09-23 14:53:17 -0700

    initial commit

---
+++ AgoraDemoTests/AgoraDemoTests.swift
@@ -0,0 +1,34 @@
+//
+//  AgoraDemoTests.swift
+//  AgoraDemoTests
+//
+//  Created by Jonathan Fotland on 9/23/19.
+//  Copyright © 2019 Jonathan Fotland. All rights reserved.
+//
+
+import XCTest
+@testable import AgoraDemo
+
+class AgoraDemoTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
