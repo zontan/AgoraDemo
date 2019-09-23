b5997069396366b498861b6fe7893eedb6289e00
Jonathan Fotland <jonathan@agora.io>
2019-09-23 15:52:52 -0700

    setting up the scene

---
+++ AgoraDemo/AgoraVideoViewController.swift
@@ -0,0 +1,43 @@
+//
+//  AgoraVideoViewController.swift
+//  AgoraDemo
+//
+//  Created by Jonathan Fotland on 9/23/19.
+//  Copyright © 2019 Jonathan Fotland. All rights reserved.
+//
+
+import UIKit
+
+class AgoraVideoViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+
+    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var localVideoView: UIView!
+    @IBOutlet weak var muteButton: UIButton!
+    @IBOutlet weak var hangUpButton: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        <#code#>
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
